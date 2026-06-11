473a0d7179eb61cb0e768abf4a9dfdc24f06d1a3
begalve-lopes <begalve.927390438.00@gmail.com>
2026-06-11 12:51:47 +0100

    agora vai funcionar

--- e2e/support/utils/world.ts
+++ e2e/support/utils/world.ts
@@ -1,18 +1,24 @@
 import { setWorldConstructor } from '@cucumber/cucumber';
 import { chromium, Browser, BrowserContext, Page } from 'playwright';
 
+const isCI = !!process.env.CI;
+
 class CustomWorld {
   browser!: Browser;
   context!: BrowserContext;
   page!: Page;
 
   async init() {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({
+      headless: isCI ? true : false
+    });
+
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
   }
 
   async close() {
+    await this.context?.close();
     await this.browser?.close();
   }
 }
